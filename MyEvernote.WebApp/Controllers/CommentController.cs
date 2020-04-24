f35ad128f603973c82d3ba88515a3a9a3629a265
pcparticle <erolaxoy98@gmail.com>
2020-04-24 22:17:47 +0300

    note-modalpopup-sending-jquery

--- MyEvernote.WebApp/Controllers/CommentController.cs
+++ MyEvernote.WebApp/Controllers/CommentController.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MyEvernote.Bll;
+using MyEvernote.Entities;
 
 namespace MyEvernote.WebApp.Controllers
 {
     public class CommentController : Controller
     {
+        private NoteManager noteManager = new NoteManager();
         // GET: Comment
-        public ActionResult ShowNoteComments()
+        public ActionResult ShowNoteComments(int? id)
         {
-            return PartialView("_PartialComment");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Note note = noteManager.GetAllNotesQueryable().AsQueryable().Include(s=>s.Comment).FirstOrDefault(x=>x.Id == id);
+
+            if (note == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            
+            return PartialView("_PartialComment",note.Comment);
         }
     }
 }
